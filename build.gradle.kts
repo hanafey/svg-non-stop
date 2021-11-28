13e9ceceb486b49706bbfdecc35418dd6c2a91cf
MKH(rocket) <mike.hanafey@gmail.com>
2021-11-27 16:50:23 -0800

    Use explicit ref to shadow instead of import.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 plugins {
     kotlin("jvm") version "1.6.0"
     java
     application
-    id("com.github.johnrengelman.shadow") version "7.1.0"}
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+}
 
 group = "com.hanafey.svg-non-stop"
 version = "1.0"
@@ -27,7 +27,7 @@ tasks.getByName<Test>("test") {
 }
 
 tasks {
-    named<ShadowJar>("shadowJar") {
+    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
         archiveBaseName.set("nonstop")
         archiveClassifier.set("")
         archiveVersion.set("")
